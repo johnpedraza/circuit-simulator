88853dc2f78726d838f3f2aae56d5237cb5bde7f
John Pedraza <johnpedraza32@gmail.com>
2025-11-27 00:17:59 -0800

    small refactor

--- src/Circuit.cpp
+++ src/Circuit.cpp
@@ -13,22 +13,23 @@ export module Circuit;
 export class Circuit {
 public:
     // Constructor
-    // TODO: error checking
-    Circuit(std::string f) :netlist_ifs{f} {
-        parse_netlist();
+    Circuit(std::string netlist_filepath) {
+        parse_netlist(netlist_filepath);
     }
     
     void print_circuit() {
     }
 
 private:
-    std::ifstream netlist_ifs; // input file stream for netlist file
-
     /*
     Parse netlist into circuit data structure
     */
-    void parse_netlist() {
+    void parse_netlist(std::string netlist_filepath) {
+        // input file stream for netlist (initializing with '=' won't work)
+        std::ifstream netlist_ifs {netlist_filepath};
+
         for (std::string line; std::getline(netlist_ifs, line); ) {
+            std::cout << line << '\n';
         }
     }
 };
